56645e67515e4f31c352b1d35cb55c656bda1b04
Colton Swapp <coltonbswapp@gmail.com>
2024-10-05 13:18:49 -0600

    Initial Commit

---
+++ nest-note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nest-note
+//
+//  Created by Colton Swapp on 10/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
